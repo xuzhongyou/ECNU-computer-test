aacc6cd6cbed482003c2def9659772afd0571a1e
77QingLiu <77qingliu@gmail.com>
2018-02-08 14:26:54 +0800

    老哥，我的代码我放到ECNU上面没有通过。我自己又写了一段代码，但是我觉得速度有点慢，你看下有什么可以改进的地方

--- 1.经典入门/1.排序/1852. Ordered Fractions.py
+++ 1.经典入门/1.排序/1852. Ordered Fractions.py
@@ -1,6 +1,9 @@
 """
 @Time:2018/1/3 18:28
 @Author:xuzhongyou
+@Source:ECNU Online Judge
+@Problem:1852
+@Website:http://acm.ecnu.edu.cn/problem/1852/
 """
 # Consider the set of all reduced fractions between 0 and 1 inclusive with denominators less than or equal to N.
 # Here is the set when N=5:
@@ -26,3 +29,23 @@ for i in range(1,num):
 result = sorted(dict_.items(),key = lambda item:item[1])
 for rest in result:
     print(rest[0])
+
+# Qing's solution
+from fractions import Fraction
+from  math import gcd
+
+num = int(input())
+result = []
+for i in range(1, num+1):
+    for j in range(0, i+1):
+        if gcd(i, j) == 1:
+            result.append(Fraction(j/i))
+
+result = [str(a.limit_denominator()) for a in sorted(result)]
+for i in result:
+    if i == '0':
+        print('0/1')
+    elif i == '1':
+        print('1/1')
+    else:
+        print(i)
