a56e32212c2c8b1e00e66c00ed44f6f9de682879
77QingLiu <77qingliu@gmail.com>
2018-02-08 13:23:48 +0800

    将排序放到循环之外，以及添加Source, Problem & Website in comment

--- 1.经典入门/1.排序/1072.小强的单词本.py
+++ 1.经典入门/1.排序/1072.小强的单词本.py
@@ -1,6 +1,9 @@
 """
 @Time:2018/1/3 18:02
 @Author:xuzhongyou
+@Source:ECNU Online Judge
+@Problem:1072
+@Website:http://acm.ecnu.edu.cn/problem/1072/
 """
 # 大二的小强马上就要英语四级考试了，小强不想像别人一样拿着四级词汇书背啊背，于是他决定每天阅读几篇英语文章，并把其
 # 中需要记忆的单词写到 notobook 上，一周之后问题来了，单词太乱了，现在小强决定请学计算机的同学你帮他解决这个问题。
@@ -18,8 +21,8 @@ result = []
 num = int(input())
 while(num):
     result.append(input())
-    new_result = sorted(set(result))
     num -=1
+new_result = sorted(set(result))
 for res in new_result:
     print(res)
 
